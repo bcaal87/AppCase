0f5754f83761e5eda83327b14943e707b2a022c4
Byron Caal <caal97@gmail.com>
2023-04-01 09:33:17 -0600

    :rocket: Nueva app case

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Variables
+int option;
+double A;
+double B;
+double resultado;
+string mensaje;
+
+//Dibujar menu
+Console.WriteLine("***** CALCULADORA *****");
+Console.WriteLine("1. Sumar.");
+Console.WriteLine("2. Restar.");
+Console.WriteLine("3. Multiplicar.");
+Console.WriteLine("4. Dividir.");
+Console.WriteLine("5. Elevar a la potencia.");
+Console.Write("Seleccione una opción (1 - 5): ");
+
+
+
+
+
