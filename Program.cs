524c4fc821e3ef8a567cc63d62ace577a6c2534a
Byron Caal <caal97@gmail.com>
2023-04-01 09:37:14 -0600

    :bug:Se agregaron las entradas de variables

--- Program.cs
+++ Program.cs
@@ -15,6 +15,15 @@ Console.WriteLine("4. Dividir.");
 Console.WriteLine("5. Elevar a la potencia.");
 Console.Write("Seleccione una opción (1 - 5): ");
 
+//Guardar Variable
+option = int.Parse(Console.ReadLine());
+// Ahora pedimos los dos números.
+Console.Write("Ingrese el primer número: ");
+A = double.Parse(Console.ReadLine());
+Console.Write("Ingrese el segundo número: ");
+B = double.Parse(Console.ReadLine());
+
+
 
 
 
